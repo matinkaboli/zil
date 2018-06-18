8a003d5595a671785db96ccc678707af31bcb967
Matin Kaboli <matinkaboli@aol.com>
2018-06-18 18:35:29 +0430

    feat: add query type to graphQL

---
+++ src/graphql/index.js
@@ -0,0 +1,7 @@
+import { GraphQLSchema } from 'graphql';
+
+import query from './query';
+
+export default new GraphQLSchema({
+  query,
+});
---
+++ src/graphql/query/index.js
@@ -0,0 +1,6 @@
+import { GraphQLObjectType } from 'graphql';
+
+export default new GraphQLObjectType({
+  name: 'Query',
+  fields: {},
+});
