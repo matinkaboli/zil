e3413b1be7bbd86cf1b1f68400e8d9f453fa9c5c
Matin Kaboli <matinkaboli@aol.com>
2018-06-19 01:25:18 +0430

    feat: validate values in signup route

--- src/routers/user/signup.js
+++ src/routers/user/signup.js
@@ -3,8 +3,10 @@ import { Router } from 'express';
 import User from 'Root/models/User';
 
 import requirements from 'Root/middlewares/requirements';
-import hash from 'Root/utils/hash';
+import password from 'Root/utils/validate/password';
+import email from 'Root/utils/validate/email';
 import { hashKey } from 'Root/config';
+import hash from 'Root/utils/hash';
 
 const router = new Router();
 const reqs = requirements('name', 'email', 'password');
@@ -12,10 +14,18 @@ const reqs = requirements('name', 'email', 'password');
 router.post('/signup', reqs, async (req, res) => {
   req.body.email = req.body.email.toLowerCase();
 
+  if (!email(req.body.email)) {
+    return res.json({ type: 2, text: 0 });
+  }
+
+  if (!password(req.body.password)) {
+    return res.json({ type: 2, text: 1 });
+  }
+
   const user = await User.findOne({ email: req.body.email });
 
   if (user) {
-    return res.json({ type: 2, text: 0 });
+    return res.json({ type: 2, text: 2 });
   }
 
   const newUser = new User({
