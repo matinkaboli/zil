b6ae56ce8601d322485835b0e518f8dcca11e121
Matin Kaboli <matinkaboli@aol.com>
2018-06-23 00:41:25 +0430

    refactor: code and user models

--- src/models/Code.js
+++ src/models/Code.js
@@ -1,24 +1,20 @@
-/*
- * Usage =
- *  Verifying user account (send code to user's email)
- */
-
 import mongoose, { Schema } from 'mongoose';
 
 export default mongoose.model('Code', Schema({
-  code: {
-    trim: true,
-    type: String,
-    required: true,
-    maxlength: 300,
-  },
   user: {
     trim: true,
     ref: 'User',
     required: true,
     type: Schema.Types.ObjectId,
   },
+  code: {
+    trim: true,
+    type: Number,
+    maxlength: 10,
+    required: true,
+  },
   createdAt: {
+    trim: true,
     type: Date,
     required: true,
     default: Date.now,
--- src/models/User.js
+++ src/models/User.js
@@ -1,38 +1,11 @@
 import mongoose, { Schema } from 'mongoose';
 
-import email from 'Root/utils/validate/email';
-
 export default mongoose.model('User', Schema({
-  name: {
+  phone: {
     trim: true,
     type: String,
+    maxlength: 10,
     required: true,
-    maxlength: 100,
-  },
-  email: {
-    trim: true,
-    unique: true,
-    type: String,
-    required: true,
-    maxlength: 200,
-    lowercase: true,
-    validate: {
-      validator(v) {
-        return email(v);
-      },
-      message: 'email is invalid',
-    },
-  },
-  password: {
-    trim: true,
-    type: String,
-    required: true,
-    maxlength: 300,
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now,
   },
   verified: {
     trim: true,
@@ -41,8 +14,13 @@ export default mongoose.model('User', Schema({
     default: false,
   },
   shops: [{
+    trim: true,
     ref: 'Shop',
-    required: true,
     type: Schema.Types.ObjectId,
   }],
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
 }));
