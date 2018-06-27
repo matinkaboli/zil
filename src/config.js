3be5aefa6ea08370b90e3a3201982cea1a5dc41d
Matin Kaboli <matinkaboli@aol.com>
2018-06-27 11:11:33 +0430

    chore: change lines of config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  port: '8080',
+  port: process.env.PORT || '8080',
   db: process.env.DB || 'mongodb://0.0.0.0/hyper',
-  sessionKey: process.env.SECRET_KEY || 'QIFE564%?sKb]JTqeN0Az.9vH4ahjM1l~',
   hashKey: process.env.HASH_KEY || '*wtt_a4pv[ZZSd,+8C8V',
+  sessionKey: process.env.SECRET_KEY || 'QIFE564%?sKb]JTqeN0Az.9vH4ahjM1l~',
 };
