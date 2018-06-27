7d2a27a190840e130a3e5e4759c8531032a7a2b0
Matin Kaboli <matinkaboli@aol.com>
2018-06-27 12:32:59 +0430

    feat: remove product router

---
+++ src/routers/product/remove.js
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+
+import Product from 'Root/models/Product';
+
+import requirements from 'Root/middlewares/requirements';
+
+const router = new Router();
+
+const reqs = requirements({
+  type: 'id',
+  required: true,
+});
+
+router.post('/product/remove', reqs, async (req, res) => {
+  try {
+    const product = await Product.findById(req.body.id);
+
+    if (!product) {
+      return res.json({ statusCode: 404, entity: 'product' });
+    }
+
+    await product.remove();
+
+    return res.json({ type: 200 });
+  } catch (error) {
+    return res.json({ statusCode: 520, error });
+  }
+});
+
+export default router;
