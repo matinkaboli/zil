ff740f4776f6c5a911fdba3dae3e3ed486ac6c43
Matin Kaboli <matinkaboli@aol.com>
2018-06-27 12:44:17 +0430

    refactor: login route with HTTP responses

--- src/routers/user/login.js
+++ src/routers/user/login.js
@@ -7,19 +7,29 @@ import Attempt from 'Root/models/Attempt';
 import requirements from 'Root/middlewares/requirements';
 
 const router = new Router();
-const reqs = requirements('phone', 'code');
+
+const reqs = requirements(
+  {
+    value: 'phone',
+    required: true,
+  },
+  {
+    value: 'code',
+    required: true,
+  },
+);
 
 router.post('/login', reqs, async (req, res) => {
   const user = await User.findOne({ phone: req.body.phone });
 
   if (!user) {
-    return res.json({ type: 2, text: 0 });
+    return res.json({ statusCode: 404, entity: 'user' });
   }
 
   const code = await Code.findOne({ user: user._id });
 
   if (!code) {
-    return res.json({ type: 2, text: 1 });
+    return res.json({ statusCode: 404, entity: 'code' });
   }
 
   let attempt = await Attempt.findOne({ user: user._id });
@@ -37,16 +47,16 @@ router.post('/login', reqs, async (req, res) => {
   attempt.save();
 
   if (attempt.attempts > 10) {
-    return res.json({ type: 2, text: 2 });
+    return res.json({ statusCode: 429 });
   }
 
   if (req.body.code === code.phone) {
     code.remove();
 
-    return res.json({ type: 0 });
+    return res.json({ statusCode: 200 });
   }
 
-  return res.json({ type: 2, text: 3 });
+  return res.json({ statusCode: 498, entity: 'code' });
 });
 
 export default router;
