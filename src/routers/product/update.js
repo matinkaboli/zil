e137412a32c8b70938bdfde2661b7e4ff32a2bd2
Matin Kaboli <matinkaboli@aol.com>
2018-06-27 13:26:44 +0430

    feat: add update route

---
+++ src/routers/product/update.js
@@ -0,0 +1,52 @@
+import { Router } from 'express';
+
+import Product from 'Root/models/Product';
+
+import requirements from 'Root/middlewares/requirements';
+
+const router = new Router();
+
+const reqs = requirements(
+  {
+    value: 'id',
+    required: true,
+  },
+  {
+    value: 'name',
+    required: true,
+  },
+  {
+    value: 'isbn',
+    required: false,
+  },
+  {
+    value: 'expiration',
+    required: false,
+  },
+  {
+    value: 'manufacturer',
+    required: false,
+  },
+);
+
+router.post('/product/update', reqs, async (req, res) => {
+  try {
+    const product = await Product.findById(req.body.id);
+
+    if (!product) {
+      return res.json({ statusCode: 404, entity: 'product' });
+    }
+
+    for (const key of Object.keys(product)) {
+      product[key] = req.body[key] || product[key] || '';
+    }
+
+    await product.save();
+
+    return res.json({ statusCode: 200 });
+  } catch (error) {
+    return res.json({ statusCode: 520, error });
+  }
+});
+
+export default router;
