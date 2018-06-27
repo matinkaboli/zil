57fb65c6170a4fa394dee3a8bcf909aa728a716d
Matin Kaboli <matinkaboli@aol.com>
2018-06-27 12:43:54 +0430

    style: replace some lines'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   extends: 'airbnb-base',
   rules: {
-    'no-restricted-syntax': 0,
-    'import/no-unresolved': 0,
-    'no-underscore-dangle': 0,
-    'no-confusing-arrow': 0,
-    'import/extensions': 0,
-    'arrow-parens': 0,
     'no-console': 0,
+    'arrow-parens': 0,
+    'import/extensions': 0,
+    'no-confusing-arrow': 0,
+    'no-underscore-dangle': 0,
+    'import/no-unresolved': 0,
+    'no-restricted-syntax': 0,
   },
 };
