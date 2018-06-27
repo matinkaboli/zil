89f35ffc6bfaf77c825f24588a31afdea1189060
Matin Kaboli <matinkaboli@aol.com>
2018-06-27 10:19:05 +0430

    refactor: product model

--- src/models/Product.js
+++ src/models/Product.js
@@ -7,43 +7,18 @@ export default mongoose.model('Product', Schema({
     required: true,
     maxlength: 100,
   },
-  weight: {
-    trim: true,
-    type: String,
-    required: true,
-    maxlength: 100,
-  },
   isbn: {
     trim: true,
     type: String,
-    required: true,
     maxlength: 50,
-  },
-  price: {
-    trim: true,
-    type: Number,
-    required: true,
-    maxlength: 30,
-  },
-  off: {
-    trim: true,
-    default: 0,
-    type: Number,
-    required: true,
   },
   photos: [{
     trim: true,
     type: String,
     maxlength: 150,
   }],
-  shop: {
-    ref: 'Shop',
-    required: true,
-    type: Schema.Types.ObjectId,
-  },
   expiration: {
     type: String,
     maxlength: 100,
-    required: true,
   },
 }));
