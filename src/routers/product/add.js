0de04c9639a6552520bbd761d6bf7a1fb37cd800
Matin Kaboli <matinkaboli@aol.com>
2018-06-27 10:32:35 +0430

    fix: product add

--- src/routers/product/add.js
+++ src/routers/product/add.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-// import Product from 'Root/models/Product';
+import Product from 'Root/models/Product';
 
 import requirements from 'Root/middlewares/requirements';
 
 const router = new Router();
+
 const reqs = requirements(
   {
     value: 'name',
@@ -21,7 +22,27 @@ const reqs = requirements(
 );
 
 router.post('/product/add', reqs, async (req, res) => {
-  res.json({ type: 2 });
+  const productValues = {
+    name: req.body.name,
+  };
+
+  if (req.body.isbn) {
+    productValues.isbn = req.body.isbn;
+  }
+
+  if (req.body.expiration) {
+    productValues.expiration = req.body.expiration;
+  }
+
+  const product = new Product(productValues);
+
+  try {
+    await product.save();
+
+    return res.json({ statusCode: 200 });
+  } catch (error) {
+    return res.json({ type: 400, error });
+  }
 });
 
 export default router;
