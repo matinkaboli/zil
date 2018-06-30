2f94634ec0ca804c5767972b29de8072bd6b5b04
Matin Kaboli <matinkaboli@aol.com>
2018-06-30 09:27:49 +0430

    chore: add product in shop

---
+++ src/models/ProductInShop.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from 'mongoose';
+
+export default mongoose.model('ProductInShop', Schema({
+  product: {
+    trim: true,
+    ref: 'Product',
+    required: true,
+    type: Schema.Types.ObjectId,
+  },
+  shop: {
+    trim: true,
+    ref: 'Shop',
+    required: true,
+    type: Schema.Types.ObjectId,
+  },
+  realPrice: {
+    trim: true,
+    type: Number,
+    maxlength: 30,
+    required: true,
+  },
+  discountedPrice: {
+    trim: true,
+    type: Number,
+    maxlength: 30,
+    required: true,
+  },
+}));
