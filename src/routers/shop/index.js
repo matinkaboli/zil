4c944f5ef17a7890eda504f6220871f756a7d1fb
Matin Kaboli <matinkaboli@aol.com>
2018-06-30 10:00:45 +0430

    fix: connect routers together

--- src/routers/shop/index.js
+++ src/routers/shop/index.js
@@ -1,7 +1,11 @@
 import createShop from './create';
 import deleteShop from './delete';
+import updateShop from './update';
+import shopProducts from './products';
 
 export default [
   createShop,
   deleteShop,
+  updateShop,
+  shopProducts,
 ];
--- src/routers/shop/products/index.js
+++ src/routers/shop/products/index.js
@@ -1,1 +1,9 @@
-export default [];
+import createProductForShop from './create';
+import deleteProductForShop from './delete';
+import updateProductForShop from './update';
+
+export default [
+  createProductForShop,
+  deleteProductForShop,
+  updateProductForShop,
+];
