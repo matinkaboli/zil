c13370ad2b721aa299e080fe44027dbd412945c3
Matin Kaboli <matinkaboli@aol.com>
2018-06-30 09:48:43 +0430

    feat: update product in shop

---
+++ src/routers/shop/products/update.js
@@ -0,0 +1,45 @@
+import { Router } from 'express';
+
+import ProductInShop from 'Root/models/ProductInShop';
+import requirements from 'Root/middlewares/requirements';
+
+const router = new Router();
+
+const reqs = requirements(
+  {
+    value: '_id',
+    required: true,
+  },
+  {
+    value: 'realPrice',
+    required: false,
+  },
+  {
+    value: 'discountedPrice',
+    required: true,
+  },
+);
+
+router.post('/shop/product/update', reqs, async (req, res) => {
+  try {
+    const productInShop = await ProductInShop.findById(req.body._id);
+
+    if (!productInShop) {
+      return res.json({ statusCode: 404, entity: 'productInShop' });
+    }
+
+    const values = ['realPrice', 'discountedPrice'];
+
+    for (const key of Object.keys(values)) {
+      productInShop[key] = req.body[key] || productInShop[key] || '';
+    }
+
+    await productInShop.save();
+
+    return res.json({ statusCode: 200 });
+  } catch (error) {
+    return res.json({ statusCode: 520, error });
+  }
+});
+
+export default router;
