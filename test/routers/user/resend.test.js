b3c48e1f2af6fd6a687aaaee9dd75edd12f54189
Matin Kaboli <matinkaboli@aol.com>
2018-07-07 10:36:49 +0430

    chore: add test file for user/resend

--- test/routers/user/resend.test.js
+++ test/routers/user/resend.test.js
@@ -1,7 +1,7 @@
 const fetch = require('../../utils/fetch');
 
-test('POST /user/enter', () => {
-  fetch('/user/enter', {
+test('POST /user/resend', () => {
+  fetch('/user/resend', {
     phone: '0123456789',
   })
     .then(response => expect(response.statusCode).toBe(200));
