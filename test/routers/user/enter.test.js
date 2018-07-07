a43fbea27bbfa7f11150d1d40f115f5a1147ead6
Matin Kaboli <matinkaboli@aol.com>
2018-07-07 11:40:26 +0430

    chore: add more information to test files

--- test/routers/user/enter.test.js
+++ test/routers/user/enter.test.js
@@ -1,8 +1,18 @@
 const fetch = require('../../utils/fetch');
 
-test('POST /user/enter', () => {
-  fetch('/user/enter', {
-    phone: '0123456789',
-  })
-    .then(response => expect(response.statusCode).toBe(200));
+describe('POST /user/enter', () => {
+  test('With correct information', () => {
+    fetch('/user/enter', {
+      phone: '9127598592',
+    })
+      .then(response => expect(response.statusCode).toBe(200));
+  });
+
+  test('With wrong phone number', () => {
+    fetch('/user/enter', {
+      phone: 'this is Matin Kaboli',
+    })
+      .then(response =>
+        expect(response).toEqual({ entity: 'phone', statusCode: 422 }));
+  });
 });
--- test/routers/user/resend.test.js
+++ test/routers/user/resend.test.js
@@ -1,8 +1,28 @@
 const fetch = require('../../utils/fetch');
 
-test('POST /user/resend', () => {
-  fetch('/user/resend', {
-    phone: '0123456789',
-  })
-    .then(response => expect(response.statusCode).toBe(200));
+describe('POST /user/resend', () => {
+  test('With Correct Information', () => {
+    fetch('/user/resend', {
+      phone: '0123456789',
+    })
+      .then(response => expect(response).toEqual({ statusCode: 200 }));
+  });
+
+  test('With missing information', () => {
+    fetch('/user/resend', {})
+      .then(response => expect(response).toEqual({
+        statusCode: 417,
+        requirements: 'phone',
+      }));
+  });
+
+  test('With wrong info and no found user', () => {
+    fetch('/user/resend', {
+      phone: '9876543210',
+    })
+      .then(response => expect(response).toEqual({
+        statusCode: 404,
+        entity: 'user',
+      }));
+  });
 });
