a5e56a72194f5888e453b77e8be6ad5c76a1b15f
Matin Kaboli <matinkaboli@aol.com>
2018-07-08 10:50:13 +0430

    feat: storage destination for images

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
+import { resolve } from 'path';
+
 export default {
   port: process.env.PORT || '8080',
+  uploadDir: resolve(__dirname, 'static/uploads'),
   dbAddress: process.env.DB || 'mongodb://0.0.0.0/hyper',
   hashKey: process.env.HASH_KEY || '*wtt_a4pv[ZZSd,+8C8V',
   sessionKey: process.env.SECRET_KEY || 'QIFE564%?sKb]JTqeN0Az.9vH4ahjM1l~',
