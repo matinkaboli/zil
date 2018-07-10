03270cb4c7180628099222fa505047903f718b1b
Matin Kaboli <matinkaboli@aol.com>
2018-07-11 03:17:41 +0430

    feat: add description to the response

--- src/middlewares/requirements.js
+++ src/middlewares/requirements.js
@@ -3,7 +3,11 @@ import sanitize from 'mongo-sanitize';
 export default (...requirements) => (req, res, next) => {
   for (const requirement of requirements) {
     if (requirement.required && !(requirement.value in req.body)) {
-      return res.json({ statusCode: 417, requirement: requirement.value });
+      return res.json({
+        statusCode: 417,
+        requirement: requirement.value,
+        description: `The server needs a value named ${requirement.value} but the client did not send it.`,
+      });
     }
 
     const value = sanitize(req.body[requirement.value]);
