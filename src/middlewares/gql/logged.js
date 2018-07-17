f42bd36c1d97b637927bb67eaec009eeb408c609
Matin Kaboli <matinkaboli@aol.com>
2018-07-17 13:17:59 +0430

    chore: graphQL middlewares

---
+++ src/middlewares/gql/logged.js
@@ -0,0 +1,18 @@
+import Token from 'Root/models/Token';
+import jwt from 'Root/utils/jwt';
+
+export default token => new Promise(async (resolve, reject) => {
+  const t = await jwt.verify(token);
+
+  if (!t) {
+    return reject();
+  }
+
+  const checkDatabase = await Token.findOne({ user: t._id });
+
+  if (!checkDatabase) {
+    return reject();
+  }
+
+  return resolve(t._id);
+});
