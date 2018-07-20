cd7713ca395c07a8f0aed58f4067caffa8c257d2
Matin Kaboli <matinkaboli@aol.com>
2018-07-20 21:00:14 +0430

    feat: check fetch status

---
+++ src/utils/checkstatus.js
@@ -0,0 +1,10 @@
+export default res => {
+  if (res.status >= 200 && res.status < 300) {
+    return res;
+  }
+
+  const error = new Error(res.statusText);
+  error.res = res;
+
+  throw error;
+};
