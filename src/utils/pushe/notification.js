972a13530c69355c7614f6bd4bc8e4928617d442
Matin Kaboli <matinkaboli@aol.com>
2018-10-08 10:52:17 +0330

    feat: implement push notification

---
+++ src/utils/pushe/notification.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch';
+
+import { pushe } from 'Root/config';
+import checkStatus from 'Root/utils/checkstatus';
+
+export default (body) => new Promise((resolve, reject) => {
+  fetch(pushe.url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Token ${pushe.token}`,
+    },
+  })
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(data => resolve(data))
+    .catch(error => reject(error));
+});
