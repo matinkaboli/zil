ac264bab33b4da63560494b7ee402a1ef0b75b8a
Matin Kaboli <matinkaboli@aol.com>
2018-10-26 18:03:18 +0330

    fix: add factor property to order schema

--- src/graphql/types/Order.js
+++ src/graphql/types/Order.js
@@ -26,6 +26,9 @@ export default new GraphQLObjectType({
     user: {
       type: UserType,
     },
+    factor: {
+      type: GraphQLInt,
+    },
     status: {
       type: GraphQLString,
     },
