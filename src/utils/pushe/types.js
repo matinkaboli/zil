a95cbe694e83d34b9b822e67b128cf5c3e01cbd1
Matin Kaboli <matinkaboli@aol.com>
2018-11-01 17:15:08 +0330

    feat: add push notification types:

---
+++ src/utils/pushe/types.js
@@ -0,0 +1,13 @@
+export default {
+  order: {
+    sent: 'ORDER_SENT',
+    approved: 'ORDER_APPROVED',
+    submitted: 'ORDER_SUBMITTED',
+    cancelled: 'ORDER_CANCELLED',
+    delivered: 'ORDER_DELIVERED',
+  },
+  showcase: {
+    added: 'SHOWCASE_ADDED',
+    discounted: 'SHOWCASE_DISCOUNTED',
+  },
+};
