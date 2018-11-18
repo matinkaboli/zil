8a4950d79766e3872b5caf1c205c208990cf0283
Matin Kaboli <matinkaboli@aol.com>
2018-11-18 15:51:25 +0330

    del: remove unnecessary console.log

--- src/routers/testPush.js
+++ src/routers/testPush.js
@@ -6,8 +6,6 @@ import { pushe } from 'Root/config';
 const router = new Router();
 
 router.post('/test-push', async (req, res) => {
-  console.log(req.body.body);
-
   const response = await fetch(pushe.url, {
     method: 'POST',
     body: JSON.stringify(JSON.parse(req.body.body)),
